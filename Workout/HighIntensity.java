44ba6e0f5ff1153ee53aaaaf97644248e2271f97
xXAdrXx <89416940+xXAdrXx@users.noreply.github.com>
2023-04-24 19:35:09 -0400

    returns cals burned and date and time

--- Workout/HighIntensity.java
+++ Workout/HighIntensity.java
@@ -1,12 +1,17 @@
 package Workout;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 public class HighIntensity implements Workout {
 
     @Override
-    public int CalcCals(int duration) {
+    public String CalcCals(int duration) {
         int calsburned=10;
         int totalcalsburned=calsburned*duration;
-        return totalcalsburned; 
+        Calendar now = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("ddMMyyyyHHmm");
+        String result = df.format(now.getTime());
+        return "Total calories burned: "+totalcalsburned +"on "+result;       
     }
     
 }
--- Workout/LowIntensity.java
+++ Workout/LowIntensity.java
@@ -1,14 +1,17 @@
 package Workout;
 
-public class LowIntensity implements Workout{
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class LowIntensity implements Workout{
     @Override
-    public int CalcCals(int duration) {
+    public String CalcCals(int duration) {
         int calsburned=5;
         int totalcalsburned=calsburned*duration;
-        
-        
-        return totalcalsburned;
+        Calendar now = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("ddMMyyyyHHmm");
+        String result = df.format(now.getTime());
+        return "Total calories burned: "+totalcalsburned +"on "+result;       
     }
-    
 }
