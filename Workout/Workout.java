e16c548e65ccdfa07a4e75ced7e511e0917e47ac
Brenden Becker <dino5887@outlook.com>
2023-04-24 19:46:08 -0400

    Made workout a class

--- Workout/Workout.java
+++ Workout/Workout.java
@@ -1,8 +1,50 @@
 package Workout;
 
-public interface Workout {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-    int duration=0;
-    String Date="";
-    int CalcCals(int duration);
- }
+public class Workout {
+
+    public enum Intensity {
+        LOW, MEDIUM, HIGH
+    };
+
+    private Intensity intensity;
+    private String dateString;
+    private int caloriesBurned;
+
+    public Workout(Intensity intensity) {
+        this.intensity = intensity;
+        Calendar now = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat("ddMMyyyyHHmm");
+        this.dateString = df.format(now.getTime());
+        if(intensity == Intensity.LOW){
+            caloriesBurned = 5;
+        } else if(intensity == Intensity.MEDIUM){
+            caloriesBurned = 7;
+        } else if(intensity == Intensity.HIGH){
+            caloriesBurned = 10;
+        } else {
+            caloriesBurned = 0;
+        }
+    }
+
+    public Intensity getIntensity() {
+        return intensity;
+    }
+
+    public void setIntensity(Intensity intensity) {
+        this.intensity = intensity;
+    }
+
+    public String getDate(){
+        return dateString;
+    }
+
+    public void setDate(String dateString) {
+        this.dateString = dateString;
+    }
+
+
+}
