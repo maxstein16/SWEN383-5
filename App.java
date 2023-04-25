94dfad77c665cf8cbb5e05d461c10f90b351ff75
Max Stein <mhs8558@rit.edu>
2023-04-24 21:17:08 -0400

    added user cli

--- App.java
+++ App.java
@@ -1,11 +1,106 @@
 import Util.JSONUtils;
 import org.json.simple.parser.ParseException;
 
+import Users.User;
+
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class App {
+    static User currentUser;
+
+    public static void createUser(){
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a username: ");
+            String username = scanner.nextLine();
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter your height (in cm): ");
+            int height =  Integer.parseInt(scanner.nextLine());
+            System.out.println("Enter your weight (in kg): ");
+            Double weight = Double.parseDouble(scanner.nextLine());
+            System.out.println("Enter your date of birth (yyyy-mm-dd): ");
+            LocalDate birthDate = LocalDate.parse(scanner.nextLine());
+            User user = new User(username, name, height, weight, birthDate);
+            try {
+                jsonUtils.addUser(user);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printAllUsers() {
+        try {
+            ArrayList<User> usersList = jsonUtils.getAllUsers();
+            HashMap mapUserSelection = new HashMap();
+            int x = 1;
+            for (User user : usersList) {
+                mapUserSelection.put(x, user.getUsername());
+                x+=1;
+            }
+            for(Object key : mapUserSelection.keySet()){
+                System.out.println(key + ". " + mapUserSelection.get(key));
+            }
+            selectUser(mapUserSelection);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectUser(HashMap map) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select a user by number: ");
+        String user = scanner.nextLine();
+        for(Object key : map.keySet()){
+            if(user.contains(String.valueOf(key))){
+                currentUser = jsonUtils.getSingleUser(map.get(key).toString());
+                break;
+            }
+        }
+    }
+
+    public static void deleteUser() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a user to delete: ");
+        String user = scanner.nextLine();
+        User userToDelete = jsonUtils.getSingleUser(user);
+        jsonUtils.removeUser(userToDelete);
+    }
+
+    public static void selectMenu() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to NUTRiAPP! Please selection an option below:\n");
+        System.out.println("1. Create account");
+        System.out.println("2. Select user");
+        System.out.println("3. Delete user\n");
+
+        String option = scanner.nextLine();
+        if(option.contains("1")){
+            createUser();
+        }
+        else if(option.contains("2")){
+            printAllUsers();
+        }
+        else if(option.contains("3")){
+            deleteUser();
+        }
+        else{
+            System.out.println("Option does not exist. Please enter a different value.");
+        }
+    }
+
     private static JSONUtils jsonUtils = new JSONUtils();
     public static void main(String[] args) throws IOException, ParseException {
-        jsonUtils.test();
+        // jsonUtils.test();
+
+        selectMenu();
+
     }
 }
