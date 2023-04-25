81b267030ddfbfaadbcdb6229261e904eb4dd75c
JoshS <jjs7965@rit.edu>
2023-04-24 21:41:27 -0400

    Added more recipes json stuff.

--- Food/Recipes/Recipes.java
+++ Food/Recipes/Recipes.java
@@ -1,4 +1,19 @@
 package Food.Recipes;
 
+import Users.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recipes {
+
+    private List<Recipe> recipes = new ArrayList<>();
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(List<User> users) {
+        this.recipes = recipes;
+    }
 }
